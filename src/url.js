351bf20093eb86a7b806068e3be442dc69d759eb
Akhilesh <96287396+Akhilesh1004@users.noreply.github.com>
2024-01-24 16:26:15 +0800

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const URL = "myblogs-api:10000"
-export const IF = "myblogs-api:10000/images/"
+export const URL = "http://myblogs-api:10000"
+export const IF = "http://myblogs-api:10000/images/"
