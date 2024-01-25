43139cc10f8b5396b4eed26b64de92b727802d1f
Akhilesh <96287396+Akhilesh1004@users.noreply.github.com>
2024-01-25 15:57:44 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = ({myblog}) => {
       <div className="fixed top-0 left-0 w-full bg-white z-50 shadow-md">
           <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
             {myblog ? <h1 className="text-lg md:text-xl font-extrabold"><Link to='/'>{myblog}'s Blog</Link></h1>:
-            <h1 className="text-lg md:text-xl font-extrabold min-w-[90px]"><Link to='/'>My Blogs</Link></h1>}
+            <h1 className="text-lg md:text-xl font-extrabold min-w-[85px]"><Link to='/'>My Blogs</Link></h1>}
             {path==='/' && <div className='flex justify-center items-center space-x-0'>
               <p onClick={()=>navigate(prompt ? "?search="+prompt : '/')} className='cursor-pointer'><FaSearch /></p>
               <input onChange={(e)=>setPrompt(e.target.value)} className='outline-none px-2' placeholder='Search a post' type='text' />
